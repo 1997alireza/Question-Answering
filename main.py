a05bfc3fefea960df5ee638514c83daebe00ad74
1997alireza <torabian.alireza@gmail.com>
2018-07-21 00:21:21 +0430

    edit distance method

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-from tools import euclidean_distance
+from tools import euclidean_distance, edit_distance, find_best_match
 from xlrd import open_workbook
+
 
 input_q = input()
 wb = open_workbook('./QA-samples.xlsx')
 sheet = wb.sheet_by_index(1)
-best_index = 0
-best_score = euclidean_distance(input_q, str(sheet.cell(0, 0).value))
-for row in range(1, sheet.nrows):
-    q = str(sheet.cell(row, 0).value)
-    ed = euclidean_distance(input_q, q)
-    if best_score > ed:
-        best_score = ed
-        best_index = row
-# print(best_index)
-print(wb.sheet_by_index(0).cell(best_index, 1))
-# print(wb.sheet_by_index(1).cell(best_index, 0))
-
-
+q_col = 0
+best_index = find_best_match(sheet, 0, input_q, euclidean_distance)
+print('Answer: ', wb.sheet_by_index(0).cell(best_index, 1))
+print()
+print('Best match: ', wb.sheet_by_index(1).cell(best_index, 0))
--- tools.py
+++ tools.py
@@ -9,3 +9,42 @@ def euclidean_distance(str1, str2):
         for i in range(len(str1), len(str2)):
             s += ord(str2[i])**2
     return s**(1/2.0)
+
+
+def edit_distance(str1, str2):
+    # w_ins == w_del == w_sub
+    l1 = len(str1)
+    l2 = len(str2)
+    diff = [[0 for i in range(0, l2+1)] for j in range(0, l1+1)]
+    diff[0][0] = 0
+    for i in range(1, l1+1):
+        diff[i][0] = i
+    for i in range(1, l2+1):
+        diff[0][i] = i
+    for i in range(1, l1+1):
+        for j in range(1, l2+1):
+            if str1[i-1] == str2[j-1]:
+                diff[i][j] = diff[i-1][j-1]
+            else:
+                diff[i][j] = min(
+                    diff[i-1][j],
+                    diff[i][j-1],
+                    diff[i-1][j-1]
+                ) + 1
+    return diff[l1][l2]
+
+
+def find_best_match(sheet, q_col, question, distance_function=euclidean_distance):
+    best_index = 0
+    best_score = distance_function(question, str(sheet.cell(0, q_col).value))
+    for row in range(1, sheet.nrows):
+        q = str(sheet.cell(row, q_col).value)
+        ed = distance_function(question, q)
+        if best_score > ed:
+            best_score = ed
+            best_index = row
+    # print(best_index)
+    return best_index
+
+
+
